9a1412aa0fcfc31b526774dbbeea23c245b21d92
Shengjun Xu <enigmashroom@gmail.com>
2018-11-19 18:04:40 +0000

    codes on Pi

--- receive_data.py
+++ receive_data.py
@@ -5,8 +5,21 @@ import time
 import picamera
 import picamera.array
 import sys
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(7, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+photo_num = int(sys.argv[1])+1
 
-
+# Initialize camera
+print "initializing camera"
+camera = picamera.PiCamera()
+camera.resolution = (int(sys.argv[2]), int(sys.argv[3]))
+rawCapture = picamera.array.PiRGBArray(camera)
+# Let camera warm up
+print "warming up 2 secs"
+time.sleep(2)
+print "set camera"
 """
 camera.sharpness = 0
 camera.contrast = 0
@@ -24,20 +37,31 @@ camera.rotation = 0
 camera.hflip = False
 camera.vflip = False
 camera.crop = (0.0, 0.0, 1.0, 1.0)
-
-camera.shutter_speed = camera.exposure_speed
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
 """
-print "stabilization 5 secs"
-time.sleep(5)
+#camera.shutter_speed = camera.exposure_speed
+#camera.exposure_mode = 'off'
+#g = camera.awb_gains
+#camera.awb_mode = 'off'
+#camera.awb_gains = g
+
+print "stabilization 10 secs"
+time.sleep(10)
 
 camera.start_preview(fullscreen=False,window=(200,20,640,480))
 path = "/home/pi/img/" + str(time.time())
 os.makedirs(path)
 i = 0
+
+#for i in range(0,photo_num):
+#    print i
+#    GPIO.output(7, True)
+#    print "signal sent"
+#    img_path = path + "/img%02d.jpg" %i
+#    camera.capture(img_path)
+#    print "photo taken"
+#    time.sleep(0.5)
+#    GPIO.output(7, False)
+#    time.sleep((2))
 
 while True:
     channel = GPIO.wait_for_edge(7, GPIO.RISING)
@@ -45,18 +69,22 @@ while True:
         print "low voltage nothing happened"
         time.sleep(0.33)
     else:
-    	print "signal received"
-    	img_path = path + "/img%02d.png" %i
-    	print i
-    	camera.capture(img_path)
-    	time.sleep(2)
-    	i += 1
+        print "signal received"
+        print i
+        img_path = path + "/img%02d.jpg" %i
+        print img_path
+        camera.capture(img_path)
+        print "finished"
+        i += 1
+        time.sleep(2)
+        print "sleep done"
 
     if i == photo_num:
         print(photo_num)
         print "photos got"
         break
 
+# GPIO.add_event_detect(3)
 
 """""
 def my_callback(channel):
@@ -68,32 +96,6 @@ GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback)
 """""
 camera.stop_preview()
 
-
-def camera_warmup():
-    # Initialize camera
-    print "initializing camera"
-    camera = picamera.PiCamera()
-    camera.resolution = (int(sys.argv[2]), int(sys.argv[3]))
-    rawCapture = picamera.array.PiRGBArray(camera)
-    # Let camera warm up
-    print "warming up 2 secs"
-    time.sleep(2)
-    print "set camera"
-
-
-def camera_capture():
-    pass
-
 camera.close()
 
 GPIO.cleanup()
-
-
-if __name__ == '__main__':
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-
-    GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    photo_num = int(sys.argv[1])
-
-    while
